1a62087a8dd7c9dc49bfdade5f445dced3d54b69
Siddhartha Dhar Choudhury <sdharchou@gmail.com>
2017-10-07 03:59:50 +0530

    home

---
+++ new/home.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>home</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+    <!--jQuery 1.12.4-->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+
+    <style type="text/css">
+
+        .navbar-brand:hover {
+
+            font-size: 150%;
+
+        }
+
+        .nav-text:hover {
+
+          font-size: 110%;
+
+        }
+
+        .nav-text {
+
+            color: #E0A108;
+
+        }
+        #x1 {
+
+            float:left;
+            background-color: #BDE0FF; /* Green */
+            font-size:15px;
+             border: 1px solid transparent;
+            border-radius:0px;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+
+
+        }
+        #x1 {
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+}
+
+#x1:hover {
+    background-color: #FF3D0A; /* Green */
+    color: white;
+}
+
+        #x2 {
+
+                        background-color: #BDE0FF; /* Green */
+            font-size:15px;
+             border: 1px solid transparent;
+            border-radius:0px;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+        }
+        #x2 {
+             -webkit-transition-duration: 0.4s; /* Safari */
+            transition-duration: 0.4s;
+           }
+
+        #x2:hover {
+        background-color: #FF3D0A; /* Green */
+         color: white;
+           }
+             #donate {
+                                        
+                background: none repeat scroll 0 0 #FFD600;
+                border: 1px solid transparent;
+                border-radius: 4px;
+                color: #000;
+                font-size: 14px;
+                font-weight: bold;
+                line-height: 1.71429;
+                margin-bottom: 0;
+                margin-top: 15px;
+                padding: 6px 12px;
+                text-align: center;
+                text-transform: uppercase;
+                vertical-align: middle;
+                            
+                            }
+                            #donate span {
+                cursor: pointer;
+                display: inline-block;
+                position: relative;
+                transition: 0.5s;
+                }
+
+                #donate span:after {
+                content: '\00bb';
+                position: absolute;
+                opacity: 0;
+                top: 0;
+                right: -20px;
+                transition: 0.5s;
+                }
+
+                #donate:hover span {
+                padding-right: 25px;
+                }
+
+                #donate:hover span:after {
+                opacity: 1;
+                right: 0;
+                }
+                #mar {
+                    text-align: justify;
+                     background-color: rgba(210,210,210,0.5);
+                }
+
+
+    </style>
+
+    <!--bttn.css-->
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/bttn.css/0.2.4/bttn.css">
+
+  </head>
+
+  <body id="page-top" style="background-color:#FFFFB2">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav" style="background-color: #FFFFD9;">
+      <div class="container heading_container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><span style="color: #FF9900;">Power in your hands</span></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about"><span class="nav-text">Health</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services"><span class="nav-text">Education</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio"><span class="nav-text">Agriculture</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Power</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Sewage</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Jobs</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Sell Crop</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Buy Crop</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Donate</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Contact</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div id="mar" style="margin:80px 100px 50px 100px";>
+
+      <h1 style="text-align:center">HOME COMPOSTING</h1>
+      <!--Image-->
+      <span class="fluid_image" style="display:block;background:url(https://d1x0je2yh2wyb8.cloudfront.net/1/5/4d7f9bf1-9980-4f9e-a3d4-37bc1661b3dc.WyI4OTJ4NDUwIiwiY3JvcCJd.jpg) no-repeat 50% 50%; width:100%; max-width:500px; height:225px"></span>
+      <h2 style="margin-top:10px">Turning waste into useful compost</h2>
+      <p style="text-size:15px";>Practical Action recognises the positive benefits of composting for poor families. With better management of waste, households can produce rich compost that can be used on gardens to improve the soil, thereby increasing productivity. This provides poor families with a variety of fresh vegetables to eat, as well as a small income from the surplus vegetables.</p>
+      <p style="text-size:15px";>Therefore, Practical Action has introduced home-composting bins into poor neighbourhoods.</p>
+      <!--How it works-->
+      <h2><button id="x1" class="color1 tabs">How it works</button></h2>
+      <h2><button id="x2" class="color1 tabs">Impact</button></h2>
+      <div class="t1";>
+      <p>Composting reduces the volume of generated wastes that would have to be transported and disposed of. It offers several benefits such as enhanced soil fertility and soil health-thereby increased agricultural productivity, improved soil biodiversity, reduced ecological risks and a better environment.</p>
+      <p>Composting is the biodegradation of organic matter under aerobic conditions where materials are transformed into compost. Dead leaves, kitchen wastes and vegetables are considered as organic wastes. Compost is odorless and pathogen free brown mixture that is specially used as soil conditioner, if it accurately decomposed.</p>
+      <p>Home composting is now being encouraged as a means of reducing the organic waste being discarded and sent to the landfills. These organic substances are bulky to handle and contribute to numerous liquid and gaseous emissions that deteriorate dumpsite environments. A good home composting programmes can significantly reduce the quantity of organic waste adding to the mainstream and subsequent emissions upon final disposal. Valuable products (compost) are produced while reducing the costs incurred for collection, transportation and final disposal at dumpsite.</p>
+      <!--Image-->
+      <img src="https://d1x0je2yh2wyb8.cloudfront.net/1/5/4d835ccc-6aec-4d6f-8ab0-57b61661b3dc.WyI2MDB4NjAwIiwic2NhbGUiXQ.jpg" alt="">
+      <h2>First prize for Tulasa</h2><br>
+      <p>Tulasa Gyawali from Syauli Baazar, 10 Bharatpur, Nepal, won first prize in a national innovation fair and was asked to share her story in an international symposium for her continuous effort on utilising waste in urban agriculture.</p>
+      <p>“I was honoured when I received the first prize. In my house, I have a compost bin and pits and I practice vermin composting too. I do not throw decomposable waste; instead I convert them to resources and use them to grow organic vegetables, she smiles.”</p>
+      <p>The Integrated Sustainable Waste Management (ISWM) project in collaboration with Bharatpur Municipality had earlier distributed compost bins in Syauli Baazar. Residents of the community do not throw wastes in the street corner as before. People are making compost from organic waste. They separate plastic in their homes and they sell them to the plastic collectors.</p>
+      <p>According to Tulasa, “this is one of the positive changes from the project. Waste management training to the community has changed people’s attitude toward disposing waste. Now we think waste as a resource.”</p>
+      <p>Tulasa and other community members were taken to India for a waste management exposure visit. “The visit was educating and motivating for me and to my neighbours. We observed the good practice in Forum of Recycle and Environment (FORCE), Mumbai and now we have replicated that practice in our community.”</p>
+      <p>In Tulasa’s community, 10 households have built a masonry compost pit in order to accommodate more organic waste and to sell the compost to the local nursery and farmers. This is an example of how awareness can make a difference.</p>
+      </div>
+      <!--Impact-->
+      <div class="t2";>
+      <h2>Reducing plastic waste in Nepal</h2><br>
+      <p>Working with partner organisations, Practical Action has provided support with training activities and field visits to help households understand solid waste management systems in Nepal.</p>
+      <p>Before 2003 residents were careless about the problems associated with excessive use of plastics, and it was a common sight to see garbage lying in public areas. Since then, significant environmental improvements have been achieved by local women’s groups who have taken action to reduce the amount of waste plastic in eight areas of Bharatupur district, Nepal.</p>
+      <p>Residents have been encouraged to segregate plastic bags from other waste and push them onto a suiro, which is a metal hook with a long stem that can hold large numbers of bags so they are not scattered to the winds. Using the suiro has helped to reduce the quantities of mixed waste and the careless dumping of waste. This is not only reducing the amounts of plastic waste in the urban area but also making people aware of the environmental problems caused by plastics.</p>
+      <p>The women’s groups collect around two tonnes of plastics every month, more than a tonne of plastics can be sold to generate an income. The plastic is collected using two rickshaws that can carry 300-500 kg of recovered waste that is transported to the dumping site designated by Bharatpur municipality.</p>
+      <p>With Practical Action’s support the programme has demonstrated that an income can be generated from solid waste and other areas are now adopting the scheme.</p>
+      <button href="donate" id="donate" style="vertical-align:middle"><span>DONATE </span></button>
+      </div>
+
+    </div>
+     <script>
+        src="https://code.jquery.com/jquery-2.2.4.min.js"
+         integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+         crossorigin="anonymous"></script>
+        <script type="text/javascript">
+        $(".t1").hide();
+        $(".t2").hide();
+        $("#x1").click(function() {
+            $(".t2").hide();
+            $(".t1").show();
+        });
+        $("#x2").click(function() {
+            $(".t1").hide();
+            $(".t2").show();
+        });
+       
+        $(".color1").click(function() {
+            $(this).css("background-color","#FF3D0A");
+        });
+        $("#x1").click(function(){
+            $("#x2").css("background-color","#BDE0FF");
+        });
+        
+         $("#x2").click(function(){
+            $("#x1").css("background-color","#BDE0FF");
+        });
+       
+           
+        </script>
+   
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+  </body>
+
+</html>
