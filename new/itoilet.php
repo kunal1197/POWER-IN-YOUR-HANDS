2cbfd55ebe71d28ce179c1ef102885a627bf9978
Siddhartha Dhar Choudhury <sdharchou@gmail.com>
2017-10-07 04:00:06 +0530

    itoilet

---
+++ new/itoilet.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>itoilet</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+    <!--jQuery 1.12.4-->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+
+    <style type="text/css">
+
+        .navbar-brand:hover {
+
+            font-size: 150%;
+
+        }
+
+        .nav-text:hover {
+
+          font-size: 110%;
+
+        }
+
+        .nav-text {
+
+            color: #E0A108;
+
+        }
+         .tabs {
+
+            float:left;
+            background-color: #BDE0FF; /* Green */
+            font-size:15px;
+             border: 1px solid transparent;
+            border-radius:0px;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+
+
+        }
+        .tabs {
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+}
+
+.tabs:hover {
+    background-color: #FF3D0A; /* Green */
+    color: white;
+}
+
+        .tab {
+
+           background-color: #BDE0FF; /* Green */
+            font-size:15px;
+             border: 1px solid transparent;
+            border-radius:0px;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+
+        }
+        .tab {
+             -webkit-transition-duration: 0.4s; /* Safari */
+            transition-duration: 0.4s;
+           }
+
+        .tab:hover {
+        background-color: #FF3D0A; /* Green */
+         color: white;
+           }
+           #donate {
+                                        
+                background: none repeat scroll 0 0 #FFD600;
+                border: 1px solid transparent;
+                border-radius: 4px;
+                color: #000;
+                font-size: 14px;
+                font-weight: bold;
+                line-height: 1.71429;
+                margin-bottom: 0;
+                margin-top: 15px;
+                padding: 6px 12px;
+                text-align: center;
+                text-transform: uppercase;
+                vertical-align: middle;
+                            
+                            }
+                            #donate span {
+                cursor: pointer;
+                display: inline-block;
+                position: relative;
+                transition: 0.5s;
+                }
+
+                #donate span:after {
+                content: '\00bb';
+                position: absolute;
+                opacity: 0;
+                top: 0;
+                right: -20px;
+                transition: 0.5s;
+                }
+
+                #donate:hover span {
+                padding-right: 25px;
+                }
+
+                #donate:hover span:after {
+                opacity: 1;
+                right: 0;
+                }
+ #mar {
+                    text-align: justify;
+                     background-color: rgba(208,208,208,0.6);
+                }
+
+    </style>
+
+    <!--bttn.css-->
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/bttn.css/0.2.4/bttn.css">
+
+  </head>
+
+  <body id="page-top" style="background-color:#FFFFB2">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav" style="background-color: #FFFFD9;">
+      <div class="container heading_container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><span style="color: #FF9900;">Power in your hands</span></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about"><span class="nav-text">Health</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services"><span class="nav-text">Education</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio"><span class="nav-text">Agriculture</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Power</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Sewage</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Jobs</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Sell Crop</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Buy Crop</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Donate</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Contact</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div id="mar" style="margin:100px 200px 0px 200px";>
+    <span class="fluid_image" style="display:block;background:url(https://d1x0je2yh2wyb8.cloudfront.net/1/6/4d832d9f-a6e4-4081-aef3-05b21661b3dc.WyI4OTJ4NDUwIiwiY3JvcCJd.jpg) no-repeat 50% 50%; width:100%; max-width:500px; height:225px"></span>
+
+      <h2>Better sanitation to reduce disease</h2>
+      <p style="font-size:15px";>In areas where water is scarce and plumbing doesn't exist, communities are forced to share communal pit latrines, which are little more than holes in the ground framed by planks of wood. The even more unhygienic alternative is called a flying toilet, which simply consists of a plastic bag; these litter the slums. Both toilets pose huge risk of disease which in many cases can prove fatal.</p>
+    <p style="font-size:15px";>Practical Action use a variety of different technologies, as appropriate to each community, to help improve sanitation and health.</p>
+    <h2><button id="x1" class="color1 tabs">How it works</button></h2>
+      <h2><button id="x2" class="color1 tabs">Impact</button></h2>
+      <h2><button id="x3" class="color1 tab">Further Info</button></h2>
+    <!--How it works -->
+    <div class="t1";>
+    <h2>Bio-latrines</h2>
+    <p>Bio-latrines use a dry toilet technology which reduces the demand for water. The bio-latrine includes a natural exhausting process so that the digester system never fills up to overflow. The waste collected in the digester is processed using anaerobic digestion to make organic manure (suitable for use as fertiliser). As the waste biodegrades, the digester captures methane gas which is used for lighting and cooking.</p>
+    <img src="https://d1x0je2yh2wyb8.cloudfront.net/b/i/4fa25770-c088-46c1-90f1-1a490ae4f5bb.WyI2MDB4NjAwIiwic2NhbGUiXQ.png" alt="">
+    <h2>Pour-flush toilets</h2>
+    <p>Pour-flush toilets are a simple technology that keeps sewage contained and off the streets. Any water left from washing or cooking, can be used to flush the toilet, and a simple water valve keeps in smells and stops flies from getting in.</p>
+    <p>The septic tanks are lined, to prevent the sewage from contaminating water supplies, and are emptied safely by trained pit emptiers, who can earn a living.</p>
+<h2>Ecological sanitation (Ecosan)</h2>
+<p>Ecological sanitation (Ecosan) is an alternative holistic approach for healthy and economically sustainable sanitation. This approach is based on the consistent implementation of the “closing the loop approach” (Nutrient Cycling), where urine and faeces are regarded as resources rather than waste. If collected separately and sanitized they can be used as organic fertilizer or as soil conditioner. It is an approach that saves water, protects water quality, prevents pollution and returns valuable nutrients to the soil.</p>
+<h2>Ventilated improved latrines (VIP)</h2>
+<p>Ventilated improved latrines (VIP) are equipped with ventilation pipes to get rid of flies and smells, and a concrete platform that’s easy to keep clean. These improved loos have proven so effective that local people have taken the initiative and built many more.</p>
+<h2>SULAV twin-pit latrines</h2>
+<p>SULAV twin-pit latrines are an effective,easy-to-maintain toilet where the waste is sealed to remove pathogens before being composted and used as manure. When one pit is full, it’s closed and the other is used. After a year, the first pit can be safely emptied and the contents used as manure.</p>
+</div>
+<!--Impact button-->
+<div class="t2";>
+<h2>Kitale, Kenya</h2>
+<p>In Kitale, Kenya, many schools faced closure - not because of governmental reforms, lack of funds or a shortage of resources - but because of their sanitation. Without clean toilets, children couldn't go to school. But just £50 is enough to help keep a school open - this is all is costs to build a Bio-latrine.</p>
+<p>Because they are a “dry” toilet, they neither require water to work (which is already scarce) nor is there any danger of waste leaching into the groundwater and polluting water supplies, causing disease.</p>
+<p>The bio-latrine system also brings with it a surprising and marvellous range of added benefits, including helping to both heat and light the school.</p>
+<p>Practical Action has already helped four schools in Kitale to install improved latrine systems, helping to keep 3,000 children in school. The benefits to the children and their schools are immense.</p>
+<p>Shimo La Tewa school in Kitale now has new bio latrines to help improve sanitation and health at the school. Methane gas generated from the latrines is used for lighting in the evenings and cooking. After the gas has been removed the bio waste is used in the school garden as a fertiliser.</p>
+<h2>Sicuani, Peru</h2>
+<p>Practical Action has introduced 300 new eco-toilets to the village of Sicuani in Peru. The eco-toilets are a dry loo (essential in an area where water is extremely scarce). They have been developed with the local communities so that the design is one which can be built readily with local materials and is easy for local people to build.</p>
+<h2>Epworth Bellapaise, Zimbabwe</h2>
+<p>Epworth Bellapaise was the epicentre for cholera outbreaks prior to 2007, largely due to the lack of suitable toilets in the area and the prevalence of open defecation. Reports from the project’s ‘Peer Educators’, (health and hygiene education promoters) indicate a significant reduction in the incidence of diarrhoea.</p>
+<p>Between January 2008 and January 2009 there was no cholera outbreak in the target area (which is hugely significant given the social, economic and infrastructural breakdown in Zimbabwe during the same period).</p>
+<p>This can, in large part, be attributed to the construction of ecosan latrines and the intense health and hygiene education carried out between September 2007 and January 2009.</p>
+<p>The ecosan toilets have combated the communities’ tendency to open defecation, which has greatly reduced the occurrence of cholera outbreaks in the area. Previously, the absence of toilets meant that families practiced open defecation and come the rainy season, faecal matter would flow into the shallow wells which the community used to collect their drinking water. It is during this process that the water borne diseases would be transmitted and outbreaks would occur. So the construction of the ecosan toilets has cut off this disease transmission route and prevented the outbreak of cholera in the project area.</p>
+</div>
+<!--How you can help-->
+<div class="t3";>
+<h2>Help transform Dharavi slums before the next rainy season</h2>
+<p>Without access to adequate toilet facilities open defecation is widespread in neighbourhood.</p>
+<p>In nine months’ time the rains will come to the settlement of Dharavi, and when it does the human waste that litters the streets will wash through the slum. Preventable diseases like cholera, diarrhoea and dysentery will bring heartbreak, despair and sometimes death.</p>
+ <p>With your support we can introduce decent sanitation to Karen’s community by implementing safe, ‘pour flush’ toilets with septic tanks – and by doing so, build and deliver healthier, safer and better futures for the families affected.</p>
+ <p>A donation from you today could help provide the simple solution that will keep the sewage in the toilets and off the streets.</p>
+      <button href="donate" id="donate" style="vertical-align:middle"><span>DONATE </span></button> 
+</div>
+ </div>
+ <!--Donate button-->
+ 
+    <script
+        src="https://code.jquery.com/jquery-2.2.4.min.js"
+         integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
+         crossorigin="anonymous"></script>
+        <script type="text/javascript">
+        $(".t1").hide();
+        $(".t2").hide();
+        $(".t3").hide();
+        $("#x1").click(function() {
+            $(".t2").hide();
+            $(".t3").hide();
+            $(".t1").show();
+        });
+        $("#x2").click(function() {
+            $(".t1").hide();
+            $(".t3").hide();
+            $(".t2").show();
+        });
+        $("#x3").click(function() {
+            $(".t1").hide();
+            $(".t2").hide();
+            $(".t3").show();
+        });
+        $(".color1").click(function() {
+            $(this).css("background-color","#FF3D0A");
+        });
+        $("#x1").click(function(){
+            $("#x2").css("background-color","#BDE0FF");
+        });
+        $("#x1").click(function(){
+            $("#x3").css("background-color","#BDE0FF");
+        });
+         $("#x2").click(function(){
+            $("#x1").css("background-color","#BDE0FF");
+        });
+         $("#x2").click(function(){
+              $("#x3").css("background-color","#BDE0FF");
+
+        });
+         $("#x3").click(function(){
+            $("#x1").css("background-color","#BDE0FF");
+        });
+         $("#x3").click(function(){
+            $("#x2").css("background-color","#BDE0FF");
+        });
+         </script>
+   
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+  </body>
+
+</html>
