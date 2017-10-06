f1360a54c986d05b1ef88034987b39ba079dd1fb
Siddhartha Dhar Choudhury <sdharchou@gmail.com>
2017-10-06 22:41:06 +0530

    time

---
+++ time.php
@@ -0,0 +1,7 @@
+<?php
+
+    $link = mysqli_connect("shareddb1c.hosting.stackcp.net","power-in-hand-313640b8","password98@","power-in-hand-313640b8");
+
+    $query = "SELECT * FROM `village` WHERE name='Medak'";
+
+?>
