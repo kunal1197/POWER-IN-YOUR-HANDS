c67ea534be637aa1b9a0fd72e96b3996588d2506
Siddhartha Dhar Choudhury <sdharchou@gmail.com>
2017-10-06 22:38:23 +0530

    plasticre

---
+++ plasticre.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>plasticre</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom fonts for this template -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+
+    <!-- Plugin CSS -->
+    <link href="vendor/magnific-popup/magnific-popup.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/creative.min.css" rel="stylesheet">
+
+    <!--jQuery 1.12.4-->
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+
+    <style type="text/css">
+
+        .navbar-brand:hover {
+
+            font-size: 150%;
+
+        }
+
+        .nav-text:hover {
+
+          font-size: 110%;
+
+        }
+
+        .nav-text {
+
+            color: #E0A108;
+
+        }
+          #x1 {
+
+            float:left;
+            background-color: #4CAF50; /* Green */
+            font-size:15px;
+             border: 1px solid transparent;
+            border-radius:0px;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+
+
+        }
+        #x1 {
+    -webkit-transition-duration: 0.4s; /* Safari */
+    transition-duration: 0.4s;
+}
+
+#x1:hover {
+    background-color: #ff1a1a; /* Green */
+    color: white;
+}
+
+        #x2 {
+
+                        background-color: #4CAF50; /* Green */
+            font-size:15px;
+             border: 1px solid transparent;
+            border-radius:0px;
+            color: white;
+            padding: 15px 32px;
+            text-align: center;
+            text-decoration: none;
+            display: inline-block;
+        }
+        #x2 {
+             -webkit-transition-duration: 0.4s; /* Safari */
+            transition-duration: 0.4s;
+           }
+
+        #x2:hover {
+        background-color: #ff1a1a; /* Green */
+         color: white;
+           }
+             #donate {
+                                        
+                background: none repeat scroll 0 0 #FFD600;
+                border: 1px solid transparent;
+                border-radius: 4px;
+                color: #000;
+                font-size: 14px;
+                font-weight: bold;
+                line-height: 1.71429;
+                margin-bottom: 0;
+                margin-top: 15px;
+                padding: 6px 12px;
+                text-align: center;
+                text-transform: uppercase;
+                vertical-align: middle;
+                            
+                            }
+                            #donate span {
+                cursor: pointer;
+                display: inline-block;
+                position: relative;
+                transition: 0.5s;
+                }
+
+                #donate span:after {
+                content: '\00bb';
+                position: absolute;
+                opacity: 0;
+                top: 0;
+                right: -20px;
+                transition: 0.5s;
+                }
+
+                #donate:hover span {
+                padding-right: 25px;
+                }
+
+                #donate:hover span:after {
+                opacity: 1;
+                right: 0;
+                }
+                .mar {
+                    margin:80px 250px 50px 150px;
+                    background-color: rgba(208,208,208,0.6);
+                }
+           
+
+
+    </style>
+
+    <!--bttn.css-->
+    <link rel = "stylesheet" href = "https://cdnjs.cloudflare.com/ajax/libs/bttn.css/0.2.4/bttn.css">
+
+  </head>
+
+  <body id="page-top" style="background-image:url('https://previews.123rf.com/images/kstudija/kstudija1110/kstudija111000019/10803639-Recycle-garbage-round-vector-background-Stock-Vector-compost-plastic-recycling.jpg');background-repeat: no-repeat;background-size: 1400px 2300px; opacity:2px;">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav" style="background-color: #FFFFD9;">
+      <div class="container heading_container">
+        <a class="navbar-brand js-scroll-trigger" href="#page-top"><span style="color: #FF9900;">Power in your hands</span></a>
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#about"><span class="nav-text">Health</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#services"><span class="nav-text">Education</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#portfolio"><span class="nav-text">Agriculture</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Power</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Sewage</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Jobs</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Sell Crop</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Buy Crop</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Donate</span></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link js-scroll-trigger" href="#contact"><span class="nav-text">Contact</span></a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+<div class="mar">
+    <div style="margin:80px 150px 50px 150px ;text-align: justify";>
+<h1 style="text-align:center;text-shadow: 2px 2px green;">PLASTIC RECYCLING</h1>
+<span class="fluid_image" style="display:block;background:url(https://d1x0je2yh2wyb8.cloudfront.net/9/2/4d7f9bb0-1934-4113-81fb-37c01661b3dc.WyI4OTJ4NDUwIiwiY3JvcCJd.jpg) no-repeat 50% 50%; width:100%; max-width:550px; height:225px; margin-top:20px;"></span>
+<!--Image-->
+<h2>Making money out of waste</h2>
+<p style="text-size:10px";>Waste plastics are often contaminated with sticky liquids and organic matter, making the sorting a dirty job and the further processing steps more difficult, and reducing the product quality.</p>
+<p>Plastics washing machines are specially designed to wash plastic solid waste materials before they are sold on for commercial recycling where plastic pellets can be turned into new produces such as fence posts.</p>
+<h2><button id="x1" class="color1 tabs">How it works</button></h2>
+      <h2><button id="x2" class="color1 tabs">Impact</button></h2>
+    
+
+<!--How it works-->
+<div class="t1";>
+<p>Several levels of washing can be achieved manually or mechanically: simply cleaning with a soft brush, washing in water with a brush or with detergent, washing in a caustic soda solution in case of resistant contaminants (oil, grease). When impurities are difficult to remove, the contaminated part of the material can be cut off.</p>
+<p>Cleaning can occur before and/or after the sorting. It is followed by manual or mechanical drying. It can consist of simply placing recovered waste to drain in the sun on a plastic sheet (to avoid dust), centrifuging shredded material or using some hot air blowing device. Plastics films can be hung on wires to save space. When left to drain, plastics material usually has to be stirred occasionally. In India, the waste plastics are washed in a large concrete basin with water pumped by a small electric engine before sorting. The washed plastics are then dried in a rotating mesh drum and spread out on the ground to dry in the sun.</p>
+<p>Converting shredded or agglomerated plastics into pellets is recommended before further processing. A pelletising machine consists of an extruder, a cooling bath and a cutting machine. During the melting caused by the extruder, the plastic can be homogenised, degassed and coloured; it is forced by a screw down the barrel through a filter screen aiming at removing solid particles and then through the extrusion die head; spaghettis are formed which are then cooled and cut into pellets. Pellets can then be used as the input materials for various moulding processes</p>
+<img src="https://d1x0je2yh2wyb8.cloudfront.net/5/9/4d7f9933-cb2c-482a-b1f1-37bc1661b3dc.WyI2MDB4NjAwIiwic2NhbGUiXQ.jpg" alt="">
+</div>
+<!--Image-->
+<div class="t2";>
+<h2>Kibera, Kenya</h2>
+<p>A scheme currently running in Kibera ‘Sustainable Management of Plastic Waste’</p>
+<p>In addition, around 4,000 plastic bags are produced every month in Kenya which are less than 15 microns in thickness. </p>
+<p>Each day, hundreds of plastic bags can be seen blowing across the slum, clogging up doorways and pit latrines, but these are now being turned into a thriving business through communities hard work.</p>
+<p>Practical Action has introduced a ‘plastic factory’ for residents and trained members on running a business and how to use the plastic washing machines effectively.</p>
+<p>Residents sell their waste plastic to community members who have formed a Recyclers Co-operative Savings and Credit Society at 5 ksh per kilogram, which has so far collected more than 30 tones of plastic bags.</p>
+<p>The plastics’ washing machine, a manual and motorized machine is specially designed to wash plastic bags before they are sold on for commercial recycling where plastic pellets can be turned into new produces.</p>
+<p>As well as recycling plastic bags, the machines can be used to wash other plastics which can then be used to make items such as fence posts.</p>
+<p>It costs as little as 13pence to process 1,000 plastic bags in the recycling factory and £1.22 to recycle plastics for the fences. </p>
+<p>Through training sessions held by Practical Action, community members have now been trained on business development and also how to use the new technology in the form of the washing machines</p>
+<p>At its launch, community members and stakeholders showed products made from the recycling scheme to show to others how waste could be recycling into improving livelihoods and therefore sustain a better quality of life. Acting as a blue print for other communities, the group hopes this will show how utilising the excessive surplus of waste plastic and contribute to sustainable development, help to alleviate poverty and environmental degradation.</p>
+    <p>These simple, yet effective projects show how working directly with communities and using what they have around them can have positive impacts on people’s lives. Taking these small steps can have major benefits for people and their families both now and in the future; it also proves E F Schumacher’s belief that ‘small is beautiful’.</p>
+    <img src="https://d1x0je2yh2wyb8.cloudfront.net/5/9/4d7f98db-eabc-44f9-9ee5-37bc1661b3dc.WyI2MDB4NjAwIiwic2NhbGUiXQ.jpg" alt="">
+    </div>
+    </div>
+    </div>
+
+
+
+    <!--Image-->
+    
+      
+
+    
+   <script type="text/javascript">
+        $(".t1").hide();
+        $(".t2").hide();
+        $("#x1").click(function() {
+            $(".t2").hide();
+            $(".t1").show();
+        });
+        $("#x2").click(function() {
+            $(".t1").hide();
+            $(".t2").show();
+        });
+       
+        $(".color1").click(function() {
+            $(this).css("background-color","red");
+        });
+        $("#x1").click(function(){
+            $("#x2").css("background-color","green");
+        });
+        
+         $("#x2").click(function(){
+            $("#x1").css("background-color","green");
+        });
+        </script>
+   
+    </div>
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/popper/popper.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="vendor/scrollreveal/scrollreveal.min.js"></script>
+    <script src="vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
+
+    <!-- Custom scripts for this template -->
+    <script src="js/creative.min.js"></script>
+
+  </body>
+
+</html>
