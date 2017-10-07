3201535d6fec9c1a2667ec2b9bebc74783339595
Siddhartha Dhar Choudhury <sdharchou@gmail.com>
2017-10-07 13:13:16 +0530

    index

---
+++ new/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Power In Your Hands</title>
+  <link href='https://fonts.googleapis.com/css?family=Raleway:200,400,800' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  <link rel='stylesheet prefetch' href='https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/css/demo.css'>
+
+      <link rel="stylesheet" href="animation/css/style.css">
+
+  
+</head>
+
+<body>
+  <div id="large-header" class="large-header">
+  <canvas id="demo-canvas"></canvas>
+    <h1 class="main-title">WELCOME TO<br><span class="thin">   Power In Your Hands</span></h1>
+</div>
+  <script src='https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/js/EasePack.min.js'></script>
+<script src='https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/js/rAF.js'></script>
+<script src='https://www.marcoguglie.it/Codepen/AnimatedHeaderBg/demo-1/js/TweenLite.min.js'></script>
+
+    <script  src="animation/js/index.js"></script>
+
+<script>
+
+window.setTimeout(function(){
+	window.location.href="index1.php";
+},2000);
+
+</script>	
+	
+	
+</body>
+</html>
